1319888ed46211e7585303adb222e1ca0b051ae3
Thanh Nguyen <wantan@iamthanh.com>
2016-10-07 02:25:56 +0200

    Removed comments

--- background.js
+++ background.js
@@ -1,19 +1,14 @@
-
 var globaltest
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        //   console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension')
-      console.log(request)
-      if (request.action == 'download') {
-            //   sendResponse({farewell: 'goodbye'})
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request)
+    if (request.action == 'download') {
         console.log(globaltest)
-        chrome.downloads.download({ url: globaltest })
+        chrome.downloads.download({url: globaltest})
         sendResponse({message: 'downloading'})
-      }
-      if (request.src) {
+    }
+    if (request.src) {
         globaltest = request.src
         sendResponse({message: globaltest})
-      }
     }
-)
+})
--- content_script.js
+++ content_script.js
@@ -1,36 +1,26 @@
 if (window == top) {
-  window.addEventListener('keyup', doKeyPress, false) // add the keyboard handler
+    window.addEventListener('keyup', doKeyPress, false)
 }
 
-trigger_key = 71 // g key
-function doKeyPress (e) {
-  // console.log(srcElement)
-  // if (e.shiftKey && e.keyCode == trigger_key) { // if e.shiftKey is not provided then script will run at all instances of typing "G"
-  if (e.ctrlKey && e.keyCode == trigger_key) {
-    chrome.runtime.sendMessage({action: 'download'}, function (response) {
-      console.log(response.message)
-    })
-  }
-
-  // chrome.runtime.sendMessage({src: srcElement.src}, function (response) {
-  //   console.log('downloading')
-  // })
+// g key
+var trigger_key = 71
+function doKeyPress(e) {
+    if (e.ctrlKey && e.keyCode == trigger_key) {
+        chrome.runtime.sendMessage({
+            action: 'download'
+        }, function(response) {
+            console.log(response.message)
+        })
+    }
 }
 
-document.addEventListener('mousemove', function (e) {
-  var srcElement = e.srcElement
-  if (srcElement.nodeName == 'IMG') {
-    chrome.runtime.sendMessage({src: srcElement.src}, function (response) {
-      console.log(response.message)
-    })
-  }
-  // Lets check if our underlying element is a DIV.
-  // if (srcElement.nodeName == 'IMG') {
-    // console.log(srcElement.src)
-    // chrome.downloads.download({url: srcElement.src, filename: 'test.jpg'})
-    // alert('ok')
-  // chrome.downloads.download({
-  //   url: 'https://backgrounds.wetransfer.net/alvpen3gl_1680x1050.jpg'
-  // })
-  // }
+document.addEventListener('mousemove', function(e) {
+    var srcElement = e.srcElement
+    if (srcElement.nodeName == 'IMG') {
+        chrome.runtime.sendMessage({
+            src: srcElement.src
+        }, function(response) {
+            console.log(response.message)
+        })
+    }
 }, false)
