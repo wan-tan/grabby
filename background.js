e377998a7f6f559f01b1eb66457dd6e53be0d184
Thanh Nguyen <wantan@iamthanh.com>
2017-10-30 23:17:41 +0100

    upadated

--- background.js
+++ background.js
@@ -1,15 +1,18 @@
 var selected_element
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action == 'download') {
-        console.log(selected_element)
-        chrome.downloads.download({url: selected_element})
-        sendResponse({message: 'downloading'})
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+
+    if (request.action == 'hover') {
+      selected_element = request.src
+      sendResponse({message: selected_element})
     }
 
-    // Get hovered element
-    if (request.action == 'hover') {
-        selected_element = request.src
-        sendResponse({message: selected_element})
+    if (request.action == 'download') {
+      chrome.downloads.download({url: selected_element})
+      sendResponse({message: 'downloading ' + selected_element})
     }
-})
+
+  }
+)
--- content_script.js
+++ content_script.js
@@ -2,36 +2,66 @@ if (window == top) {
     window.addEventListener('keyup', doKeyPress, false)
 }
 
-var trigger_key = 71 // "g" key
-var trigger_key = 89 // "y" key
+// var trigger_key = 71 // "g" key
+// var trigger_key = 89 // "y" key
+
+var trigger_key = 18 // "alt" key
+
 function doKeyPress(e) {
-    if (e.ctrlKey && e.keyCode == trigger_key) {
+    if (e.keyCode == trigger_key) {
+        // if (e.ctrlKey && e.keyCode == trigger_key) {
         chrome.runtime.sendMessage({
             action: 'download'
         }, function(response) {
-            console.log(response.message)
+            // console.log(response.message)
         })
     }
 }
 
 document.addEventListener('mousemove', function(e) {
     var srcElement = e.srcElement
+    // console.log(srcElement)
     var srcURL
+
     switch (srcElement.nodeName) {
+
+        case "DIV":
+          if(document.domain == "www.instagram.com"){
+            if (srcElement.parentNode.firstChild.firstChild.firstChild){
+                srcURL = srcElement.parentNode.firstChild.firstChild.firstChild.src
+            }
+
+            // console.log(srcElement.parentNode.firstChild)
+          } else {
+            // console.log("unknown host");
+          }
+          break;
+
         case "IMG":
-            srcURL = srcElement.src
+            srcURL = srcElement.src;
             break;
+
         case "VIDEO":
-            srcURL = srcElement.childNodes[0].src
-            break;
+          srcURL = srcElement.src;
+          if (!srcURL) {
+            srcURL = srcElement.childNodes[0].src;
+          }
+          break;
     }
-    // console.log(srcElement.childNodes[0].src)
-    // if (srcElement.nodeName == 'IMG') {
-    chrome.runtime.sendMessage({
-        action: "hover",
-        src: srcURL
-    }, function(response) {
-        console.log(response.message)
-    })
-    // }
+
+    // console.log("contentscript: " + srcURL);
+
+
+    if (srcURL == null || undefined){
+      //nothing
+      // console.log("no image found");
+    } else {
+      // console.log("image found");
+      chrome.runtime.sendMessage({
+          action: "hover",
+          src: srcURL
+      }, function(response) {
+        // console.log("response: " + response.message);
+      })
+    }
 }, false)
